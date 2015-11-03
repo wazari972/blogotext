105cfeedabbb4bb8392be9ead939186350e7f5e9
Kevin Pouget <kevin@pouget.me>
2015-11-03 13:16:26 +0100

    add missing function

--- inc/util.php
+++ inc/util.php
@@ -16,6 +16,23 @@ function redirection($url) {
 	exit;
 }
 
+    function http_parse_headers( $header )
+    {
+        $retVal = array();
+        $fields = $header; //$fields = explode("\r\n", preg_replace('/\x0D\x0A[\x09\x20]+/', ' ', $header));
+        foreach( $fields as $field ) {
+            if( preg_match('/([^:]+): (.+)/m', $field, $match) ) {
+                $match[1] = preg_replace('/(?<=^|[\x09\x20\x2D])./e', 'strtoupper("\0")', strtolower(trim($match[1])));
+                if( isset($retVal[$match[1]]) ) {
+                    $retVal[$match[1]] = array($retVal[$match[1]], $match[2]);
+                } else {
+                    $retVal[$match[1]] = trim($match[2]);
+                }
+            }
+        }
+        return $retVal;
+    }
+
 /// DECODAGES //////////
 
 function get_id($file) {
