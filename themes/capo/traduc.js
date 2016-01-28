544614c1d8db3c2f92134edbb8764f8a5416c3df
Kevin Pouget <kevin@pouget.me>
2016-01-28 18:50:25 +0100

    add side by side to capoeira theme

--- themes/capo/traduc.js
+++ themes/capo/traduc.js
@@ -11,8 +11,10 @@ $(function() {
     var rest = str.splice(2).join(SEP)
     
     $(this).html(
-        '<div class="capoeira-chant">' + before + '</div>' + SEP +
-            '<div class="capoeira-traduction">' + tr + '</div>' +
-            SEP + rest);
+	'<div>' +
+          '<div class="capoeira-chant">' + before + '</div>' + 
+          '<div class="capoeira-traduction">' + tr + '</div>' +
+	'<div class="capoeira-break"></div> </div>' + SEP + 
+        '<div class="capoeira-after">' + rest + '</div>');
     })
 })
